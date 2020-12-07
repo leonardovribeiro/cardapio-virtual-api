66fd16c2cd229d9d5b1536fd8f1f276a4f7bf9d3
Leonardo Victor Ribeiro <leonardovictor@outlook.com>
2020-12-07 10:09:08 -0300

    Mudança no nome de alguns metodos de Customer e remoçao de algumas rotas

--- api/src/router/routes/routes.go
+++ api/src/router/routes/routes.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"cardapio-virtual-api/src/middlewares"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -8,19 +9,24 @@ import (
 
 // Route representa uma rota
 type Route struct {
-	URI      string
-	Method   string
-	Function func(http.ResponseWriter, *http.Request)
-	Auth     bool
+	URI          string
+	Method       string
+	Function     func(http.ResponseWriter, *http.Request)
+	RequiredAuth bool
 }
 
 // Configure coloca todas as rotas dentro do router
 func Configure(r *mux.Router) *mux.Router {
 	routes := customersRoutes
-	routes = append(routes, routeCustomersLogin)
+	routes = append(routes, loginRoutes...)
+	routes = append(routes, productsRoutes...)
 
 	for _, route := range routes {
-		r.HandleFunc(route.URI, route.Function).Methods(route.Method)
+		if route.RequiredAuth {
+			r.HandleFunc(route.URI, middlewares.Logger(middlewares.Authenticate(route.Function))).Methods(route.Method)
+		} else {
+			r.HandleFunc(route.URI, middlewares.Logger(route.Function)).Methods(route.Method)
+		}
 	}
 
 	return r
